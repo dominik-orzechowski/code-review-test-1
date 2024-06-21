e8912aa53dc519996a3275869f635610276522e4
Dominik <dominik.orzechowski@ordergroup.pl>
2024-06-21 12:15:24 +0200

    test-4

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,8 +1,13 @@
 import Image from "next/image";
 import {Text} from "@/components/Text";
+import {Fragment} from "react";
+import {InstallationFormPage} from "@/components/InstallationFormPage";
 
 export default function Home() {
-  return <Text/>
+  return <Fragment>
+    <Text></Text>
+    <InstallationFormPage/>
+  </Fragment>
   // return (
   //   <main className="flex min-h-screen flex-col items-center justify-between p-24">
   //     <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
